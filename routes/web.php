ea3b17424a7349d5fe1488775795d6b6d73035bb
WoqueFPS <woquelolontop@gmail.com>
2025-09-12 09:28:01 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    
+});
+
+Route::get('/planeten', function () {
+    return 
+    ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
 });
